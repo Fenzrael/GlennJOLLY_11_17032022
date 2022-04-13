0142b16d338ae9850b76863c95cd9229c73c1748
Glenn_JOLLY <glenn-jolly@hotmail.fr>
2022-04-13 13:49:25 +0200

    Finalisation projet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 const Header = () => {
   return (
     <div className="header">
-      <LogoHeader />
+      <LogoHeader className="header__logo" />
       <Navigation />
     </div>
   );
